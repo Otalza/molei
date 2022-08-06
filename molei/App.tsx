a124d73b065cada1b0d56b1ccb5e702833a6330f
kkoma2623 <jiwook.dev@gmail.com>
2022-08-06 21:18:45 +0900

    console, redeclare, unused-vars warning

--- molei/App.tsx
+++ molei/App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar'
 import React, { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+	console.log('hi')
 	return (
 		<View style={styles.container}>
 			<Text>i made rn app</Text>
